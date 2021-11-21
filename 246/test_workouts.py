cd084cb7d8d0f265aced0245ae190423c4097eea
odin <odinmerlinmay@gmail.com>
2021-11-20 19:53:01 -0500

    ADD 246 Solution Fix, forgot assert!

--- 246/test_workouts.py
+++ 246/test_workouts.py
@@ -8,10 +8,13 @@ from workouts import print_workout_days
                           ('upper', 'Mon, Thu'),
                           ('lower', 'Tue, Fri'),
                           ('min', 'Wed'),
-                          ('body', 'Mon, Tue, Thu, Fri')])
+                          ('body', 'Mon, Tue, Thu, Fri'),
+                          ('', 'Mon, Tue, Wed, Thu, Fri'),
+                          ('test', 'No matching workout')])
 def test_print_workout_days(capsys, workout, expected):
     print_workout_days(workout)
-    # Capture output and split on comma
+    # Capture output and strip whitespace
     # https://docs.pytest.org/en/latest/how-to/capture-stdout-stderr.html#
     out = capsys.readouterr().out.strip()
+    assert out == expected
 
