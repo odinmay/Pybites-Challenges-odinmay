b181bd77195cae171a8b1d97d41dc058369f9e41
odin <odinmerlinmay@gmail.com>
2021-11-21 21:19:16 -0500

    ADD 251 Solution

---
+++ 251/series.py
@@ -0,0 +1,54 @@
+import string
+import pandas as pd
+
+
+def basic_series() -> pd.Series:
+    """Create a pandas Series containing the values 1, 2, 3, 4, 5
+    Don't worry about the indexes for now.
+    The name of the series should be 'Fred'
+    """
+    ser = pd.Series([1, 2, 3, 4, 5], name='Fred')
+
+    return ser
+
+
+def float_series() -> pd.Series:
+    """Create a pandas Series containing the all the values
+    from 0.000 -> 1.000 e.g. 0.000, 0.001, 0.002... 0.999, 1.000
+    Don't worry about the indexes or the series name.
+    """
+    start = 0.000
+    numbers = [start]
+    while start < 1:
+        start += 0.001
+        numbers.append(round(start, 3))
+    ser = pd.Series(numbers)
+
+    return ser
+
+def alpha_index_series() -> pd.Series:
+    """Create a Series with values 1, 2, ... 25, 26 of type int64
+    and add an index with values a, b, ... y, z
+    so index 'a'=1, 'b'=2 ... 'y'=25, 'z'=26
+    Don't worry about the series name.
+    """
+    letters = string.ascii_lowercase
+    data = {num: letters[num - 1] for num in range(1, len(letters) + 1)}
+    flipped_dict = {v : k for k, v in data.items()}
+    ser = pd.Series(data=flipped_dict, index=flipped_dict.keys())
+
+    return ser
+
+
+def object_values_series() -> pd.Series:
+    """Create a Series with values A, B, ... Y, Z of type object
+    and add an index with values 101, 102, ... 125, 126
+    so index 101='A', 102='B' ... 125='Y', 126='Z'
+    Don't worry about the series name.
+    """
+    indices = list(range(101, 127))
+    ser = pd.Series(data=list(string.ascii_uppercase), index=indices)
+
+    return ser
+
+
